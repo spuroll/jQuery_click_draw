81a69c075ea81476b245a1e67779a228332a86d0
Steven Puroll <stevenpuroll@gmail.com>
2015-01-03 08:25:04 -0800

    Refactor script for brevity. Add .toggleClass().

--- script.js
+++ script.js
@@ -1,29 +1,14 @@
 $(document).ready(function() {
     $('.reset').fadeTo('fast', .50);
-});
 
-$(document).ready(function() {
     $('div').click(function() {
-        $(this).css('background-color', 'blue');
-    });
-});
-
-$(document).ready(function() {
-    $('div').click(function() {
+        $(this).toggleClass("blue");
         $('.reset').fadeTo('fast', 1);
     });
-});
 
-$(document).ready(function() {
     $('.reset').click(function() {
         $('.reset').fadeTo('fast', .50);
-    });
-});
-
-$(document).ready(function() {
-    $('.reset').click(function() {
-        $('div').css('background-color', '#4ECDC4');
         $('.reset').css('background-color', 'lightgray');
+        $('div').removeClass("blue");
     });
 });
-
